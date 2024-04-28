681c98ffda384c975abe4b814fa5dee32deb2986
elevenqtwo <elevenqtwo@gmail.com>
2024-04-28 06:08:27 +0300

    test commit

--- src/main/java/org/elevenqtwo/core/WindowManager.java
+++ src/main/java/org/elevenqtwo/core/WindowManager.java
@@ -33,7 +33,6 @@ public class WindowManager {
 
     public void init() {
         GLFWErrorCallback.createPrint(System.err).set();
-
         
         if (!GLFW.glfwInit()) {
             throw new IllegalStateException("Unable to initialize GLFW");
